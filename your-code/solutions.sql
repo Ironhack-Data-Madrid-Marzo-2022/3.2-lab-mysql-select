2eb8fbc7d305c000e0040f73dfbca63d93e7595f
sujeto123 <adrianpradomart@gmail.com>
2022-04-06 16:23:57 +0200

    Update solutions.sql

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,7 +1,6 @@
 select  
 
-titleauthor.au_id as "AUTHOR ID",
-
+authors.au_id as "AUTHOR ID",
 authors.au_lname as "LAST NAME",
 authors.au_fname as "FIRST NAME",
 title,
@@ -21,30 +20,7 @@ on titles.title_id=titleauthor.title_id
 join authors
 on titleauthor.au_id=authors.au_id
 
--- -- challenge 2 -- 
 
-select  
-
-titleauthor.au_id,
-authors.au_lname as "LAST NAME",
-authors.au_fname as "FIRST NAME",
-title,
-pub_name as publisher, 
-count(titleauthor.au_id, publisher) as "TITLE COUNT"
-
-
-
-from titles 
-
-left join publishers
-on titles.pub_id=publishers.pub_id
-
-left join titleauthor
-on titles.title_id=titleauthor.title_id
-
-join authors
-on titleauthor.au_id=authors.au_id
-group by titles.title
 
 
 
